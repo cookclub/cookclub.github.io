cd1318199048273928eb592d4a5d20c3b2f1cf45
imanshe <86017800+imanshe@users.noreply.github.com>
2025-07-28 16:10:29 -0400

    auto: deploy

--- src/main.jsx
+++ src/main.jsx
@@ -1,14 +1,16 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { SessionProvider } from 'next-auth/react'
 import { BrowserRouter } from 'react-router-dom'
 import './index.css'
 import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
+     <SessionProvider>
+       <BrowserRouter>
+         <App />
+       </BrowserRouter>
+     </SessionProvider>
   </StrictMode>,
 )
-
